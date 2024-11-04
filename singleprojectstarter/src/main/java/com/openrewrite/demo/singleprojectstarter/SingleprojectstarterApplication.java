68b7465a25eddc633e9bc75bf4a7c96c4f5772c1
Daniel Peña Perez <danipenaperez@gmail.com>
2024-11-04 13:48:39 +0100

    Initial import

---
+++ singleprojectstarter/src/main/java/com/openrewrite/demo/singleprojectstarter/SingleprojectstarterApplication.java
@@ -0,0 +1,13 @@
+package com.openrewrite.demo.singleprojectstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleprojectstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleprojectstarterApplication.class, args);
+	}
+
+}
